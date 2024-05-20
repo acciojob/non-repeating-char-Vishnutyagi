44df8657de60cf3691380bca86563bc3de827ad9
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-05-20 12:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	 let mp=new Map();
+	for(let i=0; i<str.length; i++){
+		if(mp.has(str[i])){
+			mp.set(str[i],mp.get(str[i])+1);
+		}
+		else mp.set(str[i],1);
+	}
+	for(let i=0; i<str.length; i++){
+		if(mp.get(str[i])==1) return str[i];
+		
+	}
+	return "null";
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
